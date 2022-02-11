c9a707aab839cc896b2128094a1db386f07f7e4f
wangteng <1310445488@qq.com>
2022-02-11 13:50:24 +0800

    plugin插件，热更新处理

--- src/index.js
+++ src/index.js
@@ -3,7 +3,7 @@ Author: What (1310445488@qq.com)
 index.js (c) 2022
 Desc: description
 Created:  2022-02-10T08:47:32.577Z
-Modified: 2022-02-10T10:52:12.968Z
+Modified: 2022-02-11T05:44:39.711Z
 */
 import _ from 'lodash';
 import iconPng from "./icon.jpg";
@@ -13,7 +13,7 @@ function component() {
   const element = document.createElement('div');
 
   // Lodash, currently included via a script, is required for this line to work
-  element.innerHTML = _.join(['Hello', 'webpack'], ' ');
+  element.innerHTML = _.join(['Hello', 'webpa2111ck'], ' 1212212');
   const myIcon = new Image();
   myIcon.src = iconPng;
   element.appendChild(myIcon);
--- webpack.config.js
+++ webpack.config.js
@@ -3,10 +3,11 @@ Author: What (1310445488@qq.com)
 webpack.config.js (c) 2022
 Desc: description
 Created:  2022-02-10T09:20:00.354Z
-Modified: 2022-02-10T10:46:30.290Z
+Modified: 2022-02-11T05:42:53.670Z
 */
 
 const path = require('path');
+const HtmlWebpackPlugin = require("html-webpack-plugin");
 
 module.exports = {
   entry: './src/index.js', // 打包文件的入口，webpack将从这个文件开始分析整个项目的依赖结构
@@ -14,6 +15,19 @@ module.exports = {
     filename: 'main.js', // 输出的文件名称
     path: path.resolve(__dirname, 'dist'), // 输出的文件夹
   },
+  target: "web",
+  mode: "development",
+  devtool: "inline-source-map",
+  devServer: {
+    // contentBase: path.resolve(__dirname, "dist"),
+    static : {
+      directory : path.join(__dirname, "dist")
+    },
+    hot: "only",
+    compress: true,
+    port: 3002,
+  },
+
   module: {
     rules: [
       {
@@ -31,4 +45,10 @@ module.exports = {
       },
     ],
   },
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: path.join(__dirname, "./index.html"),  // ./index.html是作为模板生成dist/index.html
+    }),
+  ],
+
 };
